724984c64a40348a6982ae30924649acf5a0994e
fasihh <fhk.tcs@gmail.com>
2024-03-05 21:44:11 +0500

    divided the draw function into 2. created object collision function.

--- src/headers/player.hpp
+++ src/headers/player.hpp
@@ -1,12 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include "entity.hpp"
+#include "object.hpp"
 #pragma once
 
 class Player : public Entity {
+protected:
+    void resolveObjectCollision(Object);
 private:
+    sf::FloatRect nextPosition;
     void movement();
 public:
     Player(float, sf::Color);
 
+    void update(Object);
     void draw(sf::RenderWindow&);
 };
