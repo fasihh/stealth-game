b8bf87f5082942b2c69d4b545b2cb7cf02640f60
fasihh <fhk.tcs@gmail.com>
2024-03-09 16:50:04 +0500

    changing object implementation

--- src/headers/object.hpp
+++ src/headers/object.hpp
@@ -1,17 +1,22 @@
 #include <SFML/Graphics.hpp>
 #pragma once
 
-class Object {
+class Object : public sf::Drawable {
 private:
-    sf::RectangleShape object;
+    sf::VertexArray vertices;
+    sf::Vector2f size;
+    sf::Vector2f position;
+    sf::Color color;
+
+    virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
+
+    void initVertices();
 public:
     Object(sf::Vector2f);
+    Object(float, float);
+
+    void setPosition(const sf::Vector2f);
+    void setPosition(const float, const float);
 
     void setColor(const sf::Color);
-    void setSize(const sf::Vector2f);
-    void setPosition(const sf::Vector2f);
-
-    sf::FloatRect getBounds() const;
-
-    void draw(sf::RenderWindow&);
 };
--- src/object.cpp
+++ src/object.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include "headers/object.hpp"
 
-Object::Object(sf::Vector2f size) : object(size) {}
-
-void Object::setColor(const sf::Color color) { object.setFillColor(color); }
-void Object::setSize(const sf::Vector2f size) { object.setSize(size); }
-void Object::setPosition(const sf::Vector2f position) { object.setPosition(position); }
-
-sf::FloatRect Object::getBounds() const { return object.getGlobalBounds(); }
-
-void Object::draw(sf::RenderWindow& window) {
-    window.draw(object);
+Object::Object(sf::Vector2f size) : size(size), position(0.f, 0.f), vertices(sf::LinesStrip, 4), color(sf::Color::White) {
+    this->initVertices();
 }
+Object::Object(float x, float y) : size(x, y), position(0.f, 0.f), vertices(sf::LinesStrip, 4), color(sf::Color::White) {
+    this->initVertices();
+}
+
+void Object::initVertices() {
+    vertices[0].position = position;
+    vertices[1].position = position + sf::Vector2f{ size.x, 0.f };
+    vertices[2].position = position + size;
+    vertices[3].position = position + sf::Vector2f{ 0.f, size.y };
+}
+
+void Object::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(vertices, states);
+}
+
+void Object::setPosition(const sf::Vector2f position) { this->position = position; }
+void Object::setPosition(const float x, const float y) { position.x = x, position.y = y; }
+
+void Object::setColor(const sf::Color color) { this->color = color; }
